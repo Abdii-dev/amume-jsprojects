3ac81100d1ab1ed4d63ab3f8ea777c7559398e59
Abdii-dev <150609234+Abdii-dev@users.noreply.github.com>
2024-02-08 00:08:54 +0300

    Add files via upload

---
+++ maths.js
@@ -0,0 +1,21 @@
+
+    //learn Math    
+var herega= 4;
+var lak = 3;
+document.write(herega+lak +"<br>");
+
+console.log(9%5 +"<br>");
+//absloute value
+const absol = -6;
+document.write(Math.abs(absol) + "<br>");
+// Lakkofsa siqsuuf 
+const number = 6.5;
+document.write(Math.round(number) +"<br>");
+//lakkofsa 
+document.write(Math.max( 8, 10, 6) + "<br>");
+
+document.write(Math.pow(10, 2) + "<br>");
+
+document.write(Math.sqrt(144) +"<br>");
+//To generet numbers
+document.write(Math.random(1-10));
